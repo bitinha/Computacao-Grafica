1fb479e2d298ee0e956a33fe40a53008818c1f6b
LFCC98 <a81960@alunos.uminho.pt>
2019-02-24 20:55:40 +0000

    Ja fiz o generator

--- generator/generator/main.cpp
+++ generator/generator/main.cpp
@@ -1,16 +1,72 @@
 #include <stdio.h>
 #include <cstdlib>
-#include "Cone.h"
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <thread>
 
+using namespace std;
+
+void writeSphereFile(string forma, const char* radius, const char* slices, const char* stacks, string name) {
+	fstream file;
+	file.open(name, ios::out);
+	if (file.is_open()) {
+		file << "Forma: " + forma;
+		file << "\nRadious: " + string(radius);
+		file << "\nSlices: " + string(slices);
+		file << "\nStacks: " + string(stacks);
+	}
+	else printf("File not opened\n");
+	file.close();
+}
+
+void writePlaneFile(string forma, const char* x, const char* z, string name) {
+	fstream file;
+	file.open(name, ios::out);
+	if (file.is_open()) {
+		file << "Forma: " + forma;
+		file << "\nX: " + string(z);
+		file << "\nZ: " + string(x);
+	}
+	else printf("File not opened\n");
+	file.close();
+}
+
+void writeBoxFile(string forma, const char* x, const char* y, const char* z, const char* divisions, string name) {
+	fstream file;
+	file.open(name, ios::out);
+	if (file.is_open()) {
+		file << "Forma: " + forma;
+		file << "\nX: " + string(x);
+		file << "\nY: " + string(y);
+		file << "\nZ: " + string(z);
+		file << "\nDivisions: " + string(divisions);
+	}
+	else printf("File not opened\n");
+	file.close();
+}
+
+void writeConeFile(string forma, const char* radius, const char* height, const char* slices, const char* stacks, string name) {
+	fstream file;
+	file.open(name, ios::out);
+	if (file.is_open()) {
+		file << "Forma: " + forma;
+		file << "\nBottom Radious: " + string(radius);
+		file << "\nHeight: " + string(height);
+		file << "\nSlices: " + string(slices);
+		file << "\nStacks: " + string(stacks);
+	}
+	else printf("File not opened\n");
+	file.close();
+}
 
 int main(int argc, const char* argv[]) {
-	printf("ola\n");
-	float f = 1;
-	if(argc > 1){
-		f = std::atof(argv[1]);
-	}
-	Cone c = Cone(f,2,1,1);
-	printf("%f",c.getRadius());
-	return 0;
-
+	string forma = string(argv[1]);
+	string fileName = string(argv[argc - 1]);
+	if (forma.compare("sphere") && argc == 6) { writeSphereFile(forma, argv[2], argv[3], argv[4], fileName); }
+	else if (forma.compare("plane") && argc == 5) { writePlaneFile(forma, argv[2], argv[3], fileName); }
+	else if (forma.compare("box") && argc == 7) { writeBoxFile(forma, argv[2], argv[3], argv[4], argv[5], fileName); }
+	else if (forma.compare("cone") && argc == 7) { writeConeFile(forma, argv[2], argv[3], argv[4], argv[5], fileName); }
+	else { printf("Argumentos inválidos\n"); std::this_thread::sleep_for(std::chrono::milliseconds(1000));	}
+	return 1;
 }
